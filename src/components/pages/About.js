3ad7c7c30aa777e74f0878c9c28dfdbdd613b3b9
Hiba243 <hibafatima24@gmail.com>
2022-04-07 13:58:00 +0530

    added responsive design

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,7 +7,9 @@ const About = () => {
             <div className="about">
                 <h1>About</h1>
                 <div>
-                    <p>View the art from Met Museum of New York. Developed by Hiba Fatima using React, react-gallery-carousel and the met collection API.</p>
+                    <p>View the art from Met Museum of New York.</p>
+                    <br/>
+                    <p>Developed by Hiba Fatima using React, react-gallery-carousel and the met collection API.</p>
                     <br/>
                     <p>Version 1.0.0</p>
                 </div>
